cf40edc60cca184645c17d15862384f711dc6056
Ahmed_Elwakad24 <116885810+AhmedElwakad@users.noreply.github.com>
2024-05-10 23:42:26 +0300

    Update weather.h

--- src/app/weather.h
+++ src/app/weather.h
@@ -1,43 +1,27 @@
-/**
- * @file weather.h
- * @brief Functions of weather measurement.
- * @author Your Name
- * @date Data of last edit
- */
+
+/*
+ * author : Ahmed Aly Hussien Elwakad
+ * version: 1.0
+ * date of last edit: 5/10/2024
+ *
+ * */
+
 
 #ifndef WEATHER_H
 #define WEATHER_H
 
-#include <stdbool.h>
-#include <stdint.h>
 
-typedef enum : uint8_t {
-  WEATHER_OK = 0,
-  WEATHER_ERROR = 1,
-} weather_status_t;
+typedef enum{
+	Weather_OK,
+	Weather_Error
+}weather_status_t;
 
-/* @brief Initializes all drivers which are used to measure weather parameters.
- *
- * This function initializes the weather monitoring system.
- *
- * @return WEATHER_OK
- */
+
+
 weather_status_t weather_init(void);
 
-/* @brief Measures weather parameters.
- *
- * This function measures temperature, humidity, and light intensity.
- *
- * @param[out] p_temperature_celisus Pointer to store the measured temperature
- *                                   in Celsius.
- * @param[out] p_humidity_percent Pointer to store the measured humidity
- *                                percentage.
- * @param[out] p_light_percent Pointer to store the measured light intensity
- *                             percentage.
- * @return WEATHER_OK
- */
-weather_status_t weather_measure(uint8_t &p_temperature_celisus,
-                                 uint8_t &p_humidity_percent,
-                                 uint8_t &p_light_percent);
+weather_status_t weather_measures(uint8_t * temperature
+		                         ,uint8_t * humidity
+								 ,uint8_t * light_percentage);
 
-#endif /* WEATHER_H */
+#endif
