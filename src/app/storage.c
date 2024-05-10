1893c7a2c6e353223a41d91b58595a17ec911c86
mgamalf <mj7719414@gmail.com>
2024-05-10 18:44:45 +0300

    weather iot storage management system

---
+++ src/app/storage.c
@@ -0,0 +1,106 @@
+/**
+ * @file storage.h
+ * @brief Functions of storage measurement.
+ *
+ * This file contains definitions for functions related to storage measurement
+ * and management.
+ * It stores any data along with current data and time automatically.
+ * It uses circular queue to handle storage in EEPROM.
+ *
+ * @author Mahmoud Gamal
+ * @date May 10 2024
+ */
+
+#include "storage.h"
+#include "../hal/ds1307.h"
+
+// Circular queue indices
+static uint8_t storage_front = 0;
+static uint8_t storage_rear = 0;
+
+// Function to calculate EEPROM address for a given block index
+static uint16_t get_eeprom_address(uint8_t block_index) {
+    if (block_index >= TOTAL_BLOCKS) {
+        return STORAGE_ERROR;
+    }
+    return STORAGE_BASE_ADDRESS + (block_index * STORAGE_BLOCK_SIZE);
+}
+
+// Function to increment a circular queue index
+static uint8_t increment_index(uint8_t index) {
+    return (index + 1) % TOTAL_BLOCKS;
+}
+
+// Function to convert BCD to integer
+static uint8_t bcd_to_int(uint8_t bcd) {
+    return ((uint8_t)((bcd >> 4) * 10)) + ((uint8_t)(bcd & 0x0F));
+}
+
+// Function to initialize storage system
+storage_status_t storage_init(void) {
+    // Initialize EEPROM
+    eeprom_busy_wait();
+    
+    return STORAGE_OK;
+}
+
+// Function to add a block of data
+storage_status_t storage_enqueue_block(const uint8_t *p_data) {
+    // Calculate timestamp
+    RTC_Time_t timestamp;
+    RTC_getTime(&timestamp);
+    uint8_t arr[7] = {bcd_to_int(timestamp->timeArr[0]), bcd_to_int(timestamp->timeArr[1]),
+                      bcd_to_int(timestamp->timeArr[2]), bcd_to_int(timestamp->timeArr[3]),
+                      bcd_to_int(timestamp->timeArr[4]), bcd_to_int(timestamp->timeArr[5]),
+                      bcd_to_int(timestamp->timeArr[6])};
+
+    // Calculate EEPROM address for the given block index
+    uint16_t eeprom_address = get_eeprom_address(storage_rear);
+    
+    // Write data to EEPROM
+    eeprom_write_block(p_data, eeprom_address, STORAGE_BLOCK_DATA_SIZE);
+    eeprom_address += STORAGE_BLOCK_DATA_SIZE;
+    
+    // Write timestamp to EEPROM
+    eeprom_write_block(arr, eeprom_address, sizeof(arr));
+    
+    // Update storage rear
+    storage_rear = increment_index(storage_rear);
+
+    return STORAGE_OK;
+}
+
+// Function to get the number of blocks stored in circular queue
+storage_status_t storage_get_length(uint8_t *p_length) {
+    // Calculate length of data stored in circular queue
+    *p_length = (storage_rear >= storage_front) ? (storage_rear - storage_front) : (TOTAL_BLOCKS - (storage_front - storage_rear));
+    
+    return STORAGE_OK;
+}
+
+// Function to get block of data
+storage_status_t storage_get_block(uint8_t index, uint8_t *p_data, timestamp_t *p_timestamp) {
+    // Check if index is valid
+    if (index >= TOTAL_BLOCKS) {
+        return STORAGE_ERROR;
+    }
+
+    // Calculate EEPROM address for the given block index
+    uint16_t eeprom_address = get_eeprom_address(index);
+    
+    // Read data from EEPROM
+    eeprom_read_block(p_data, eeprom_address, STORAGE_BLOCK_DATA_SIZE);
+    eeprom_address += STORAGE_BLOCK_DATA_SIZE;
+    
+    // Read timestamp from EEPROM
+    uint8_t arr[7];
+    eeprom_read_block(arr, eeprom_address, sizeof(arr));
+    for (int i = 0; i < 7; i++)
+    {
+    	p_timestamp->timestampArr[i] = arr[i];
+    }
+    
+    return STORAGE_OK;
+}
+
+
--- src/app/storage.h
+++ src/app/storage.h
@@ -7,8 +7,8 @@
  * It stores any data along with current data and time automatically.
  * It uses circular queue to handle storage in EEPROM.
  *
- * @author Your Name
- * @date Data of last edit
+ * @author Mahmoud Gamal
+ * @date May 10 2024
  */
 
 #ifndef STORAGE_H
@@ -17,27 +17,59 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include "avr/eeprom.h"
 
 /* The size is application dependent */
 #define STORAGE_BLOCK_DATA_SIZE 3
 
-typedef enum : uint8_t {
+// EEPROM base address
+#define STORAGE_BASE_ADDRESS 0x0000
+
+// EEPROM block size
+#define STORAGE_BLOCK_SIZE (STORAGE_BLOCK_DATA_SIZE + sizeof(timestamp_t))
+
+// EEPROM size 
+#define EEPROM_SIZE 1024
+
+// total number of blocks
+#define TOTAL_BLOCKS (EEPROM_SIZE / STORAGE_BLOCK_SIZE)
+
+typedef enum {
   STORAGE_OK = 0,
   STORAGE_ERROR = 1,
 } storage_status_t;
 
-typedef struct {
-  uint32_t utc_year : 12, utc_month : 4, utc_day : 5, utc_hour : 6;
-  uint8_t utc_minute : 6;
-} storage_timestamp_t;
+//typedef struct {
+//  uint32_t utc_year : 12, utc_month : 4, utc_day : 5, utc_hour : 6;
+//  uint8_t utc_minute : 6;
+//} storage_timestamp_t;
 
+/* Data Structure To Store Time either in an Array Or a struct
+ * where array indices access the struct fields in the same order */
+typedef union{
+	uint8_t timestampArr[7];
+	struct{
+		uint8_t seconds;
+		uint8_t minutes;
+		uint8_t hours;
+		uint8_t dayOfWeek;
+		uint8_t dayOfMonth;
+		uint8_t month;
+		uint8_t year; 
+	}timestamp;
+}timestamp_t;
+
+// Function to initialize storage system
 storage_status_t storage_init(void);
 
+// Function to add a block of data
 storage_status_t storage_enqueue_block(const uint8_t *p_data);
 
+// Function to get the number of blocks stored in circular queue
 storage_status_t storage_get_length(uint8_t *p_length);
 
+// Function to get block of data
 storage_status_t storage_get_block(uint8_t index, uint8_t *p_data,
-                                   storage_timestamp_t *p_timestamp);
+                                   timestamp_t *p_timestamp);
 
 #endif /* STORAGE_H */
