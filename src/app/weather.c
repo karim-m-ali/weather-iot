979d218ef0c4d2eb26cd65581e2e30e0e1739fc4
Ahmed_Elwakad24 <116885810+AhmedElwakad@users.noreply.github.com>
2024-05-10 23:41:10 +0300

    Update weather.c

---
+++ src/app/weather.c
@@ -0,0 +1,41 @@
+
+
+/*
+ * author : Ahmed Aly Hussien Elwakad
+ * version: 1.0
+ * date of last edit: 5/10/2024
+ *
+ * */
+
+#ifndef WEATHER_C
+#define WEATHER_C
+
+#include "weather.h"
+#include "./hal/dht11.h"
+#include "./mcal/adc.h"
+
+#define LDR_pin 0
+
+weather_status_t weather_init(void){
+  ADC_Init();
+  dht11_init();
+return Weather_OK;
+}
+
+weather_status_t weather_measures(uint8_t * temperature
+		                         ,uint8_t * humidity
+								 ,uint8_t * light_percentage){
+
+
+	if(dht11_read ( temperature, humidity)){
+		return Weather_Error;
+	}
+	if(ADC_read ( light_percentage,LDR_pin)){
+			return Weather_Error;
+		}
+
+	return Weather_OK;
+}
+
+
+#endif
